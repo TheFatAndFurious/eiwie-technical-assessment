fc930d8c21df7304f02207daa68c8f2b6daa5e86
TheFatAndFurious <mrguerrilla@gmail.com>
2025-03-20 01:27:28 +0100

    incomes module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,19 @@
-import { IsString, IsEmail, MinLength } from 'class-validator'
-import {Field, InputType} from "@nestjs/graphql";
+import { IsString, IsEmail, MinLength } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateUserDto {
-    @Field()
-    @IsString()
-    @MinLength(4)
-    username: string;
+  @Field()
+  @IsString()
+  @MinLength(4)
+  username: string;
 
-    @Field()
-    @IsEmail()
-    email: string;
+  @Field()
+  @IsEmail()
+  email: string;
 
-
-    @Field()
-    @IsString()
-    @MinLength(8)
-    password: string;
+  @Field()
+  @IsString()
+  @MinLength(8)
+  password: string;
 }
--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,24 +1,20 @@
-import { Document} from "mongoose";
-import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
-import {Field, ObjectType} from "@nestjs/graphql";
-
-// export type UserDocument = HydratedDocument<User>
-
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 @Schema({ timestamps: true })
-export class User extends Document{
+export class User extends Document {
+  @Field()
+  @Prop({ required: true, unique: true })
+  username: string;
 
-    @Field()
-    @Prop({ required: true, unique: true })
-    username: string;
+  @Field()
+  @Prop({ required: true, unique: true })
+  email: string;
 
-    @Field()
-    @Prop({required: true, unique: true})
-    email: string;
-
-    @Prop({required: true})
-    password: string;
+  @Prop({ required: true })
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
